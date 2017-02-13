62c06d6f854ac1ef864c89bc17bedc612bf4e62f
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-02-13 16:11:47 +0000

    fourth step completed

---
+++ get-and-print-html-callback.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step2.html'
+};
+
+function getHTML(options, callback){
+var body = '';
+
+  var innerCallback = function(response) {
+
+    response.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    response.on('end', function(){
+      callback(body);
+    })
+  }
+
+  https.request(requestOptions, innerCallback).end();
+}
+
+function printHTML(html){
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
